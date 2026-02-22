397bb88be5d4fe9bec339f8ced04bd6973d31b1b
Colin McDonald <89328592+1colinmcdonald@users.noreply.github.com>
2026-02-22 09:16:03 -0800

    Stuff

---
+++ src/points_and_segment.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <CGAL/Simple_cartesian.h>
+
+typedef CGAL::Simple_cartesian<double> Kernel;
+typedef Kernel::Point_2 Point_2;
+typedef Kernel::Segment_2 Segment_2;
+
+int main()
+{
+    Point_2 p(1,1), q(10,10);
+    Segment_2 s(p, q);
+    std::cout << "Segment: " << s << "\n";
+    return 0;
+}
