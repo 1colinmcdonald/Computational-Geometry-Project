8acad9be04761fbdce8e11cb8e3e02587550a61d
Colin McDonald <89328592+1colinmcdonald@users.noreply.github.com>
2026-02-22 10:03:21 -0800

    Compile

--- src/points_and_segment.cpp
+++ src/points_and_segment.cpp
@@ -13,10 +13,10 @@ vector<Point_2> graham(std::vector<Point_2> points)
     return points;
 }
 
-vector<Point_2> jarvis(std::vector<Point_2> points)
-{
-    auto v0 = numeric_limits<double>::infinity()
-}
+// vector<Point_2> jarvis(std::vector<Point_2> points)
+// {
+//     auto v0 = numeric_limits<double>::infinity();
+// }
 
 
 bool same_hull(vector<Point_2> hull1, vector<Point_2> hull2)
@@ -28,7 +28,7 @@ bool same_hull(vector<Point_2> hull1, vector<Point_2> hull2)
         return true;
 
     // Find a matching point
-    auto hull1Front = hull1.front();
+    auto hull1Front = hull1.begin();
     auto it2 = find(hull2.begin(), hull2.end(), *hull1Front);
 
     // Go through (looping) and verify all points match
@@ -41,15 +41,25 @@ bool same_hull(vector<Point_2> hull1, vector<Point_2> hull2)
         if (it2 == hull2.end())
             it2 = hull2.begin();
     }
+    return true;
 }
 
 // Tests
 #define IS_TRUE(x) { if (!(x)) std::cout << __FUNCTION__ << " failed on line " << __LINE__ << std::endl; }
 
+void test_same_hull()
+{
+    vector<Point_2> hull1 = { Point_2(0,0), Point_2(1,0), Point_2(1,1) };
+    vector<Point_2> hull2 = { Point_2(1,0), Point_2(1,1), Point_2(0,0) };
+    IS_TRUE(same_hull(hull1, hull2));
 
+    vector<Point_2> hull3 = { Point_2(0,0), Point_2(1,0) };
+    IS_TRUE(!same_hull(hull1, hull3));
+}
 
 int main()
 {
+    test_same_hull();
     std::vector<Point_2> points_list;
     Point_2 p(1,1), q(10,10);
     points_list.push_back(p);
