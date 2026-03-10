445180e0566404e96b8bdaee6a783ab896978a6f
Colin McDonald <89328592+1colinmcdonald@users.noreply.github.com>
2026-03-10 12:43:38 -0700

    Implement Graham Scan
    It might be super buggy but it seems to work for the most part on our
    inputs.

--- benchmark.cpp
+++ benchmark.cpp
@@ -28,13 +28,54 @@ typedef CGAL::Polygon_2<K> Polygon_2;
 
 bool y_comp(Point_2 a, Point_2 b)
 {
-	if (a.y() < b.y()) {
+	if (a.y() < b.y())
+	{
 		return true;
 	}
-	if (a.y() == b.y()) {
+	if (a.y() == b.y())
+	{
 		return a.x() <= b.x();
 	}
 	return false;
+}
+
+bool y_neg_x_comp(Point_2 a, Point_2 b)
+{
+	if (a.y() < b.y())
+	{
+		return true;
+	}
+	if (a.y() == b.y())
+	{
+		return a.x() > b.x();
+	}
+	return false;
+}
+
+function<bool(Point_2, Point_2)> make_upper_hull_comp(Point_2 highest)
+{
+	return [highest](Point_2 a, Point_2 b)
+	{
+		if (a.x() < b.x())
+		{
+			return true;
+		}
+		if (a.x() > b.x())
+		{
+			return false;
+		}
+		if (a.x() <= highest.x())
+		{
+			return a.y() < b.y();
+		}
+		return a.y() > b.y();
+	};
+}
+
+// vector<Point_2> jarvis_vector(const std::vector<Point_2> &points)
+Point_2 get_highest(const vector<Point_2> &points)
+{
+	return *max_element(points.begin(), points.end(), y_neg_x_comp);
 }
 
 Point_2 get_lowest(vector<Point_2> points)
@@ -42,57 +83,59 @@ Point_2 get_lowest(vector<Point_2> points)
 	return *min_element(points.begin(), points.end(), y_comp);
 }
 
-
-
 bool same_hull(vector<Point_2> hull1, vector<Point_2> hull2)
 {
-    if (hull1.size() != hull2.size())
-        return false;
+	if (hull1.size() != hull2.size())
+		return false;
 
-    if (hull1.empty())
-        return true;
+	if (hull1.empty())
+		return true;
 
-    // Find a matching point
-    auto hull1Front = hull1.begin();
-    auto it2 = find(hull2.begin(), hull2.end(), *hull1Front);
+	// Find a matching point
+	auto hull1Front = hull1.begin();
+	auto it2 = find(hull2.begin(), hull2.end(), *hull1Front);
 
-    // Go through (looping) and verify all points match
-    for (size_t i = 0; i < hull1.size(); i++)
-    {
-        if (hull1[i] != *it2)
-            return false;
+	// Go through (looping) and verify all points match
+	for (size_t i = 0; i < hull1.size(); i++)
+	{
+		if (hull1[i] != *it2)
+			return false;
 
-        it2++;
-        if (it2 == hull2.end())
-            it2 = hull2.begin();
-    }
-    return true;
+		it2++;
+		if (it2 == hull2.end())
+			it2 = hull2.begin();
+	}
+	return true;
 }
 
 // Tests
-#define IS_TRUE(x) { if (!(x)) std::cout << __FUNCTION__ << " failed on line " << __LINE__ << std::endl; }
+#define IS_TRUE(x)                                                                    \
+	{                                                                                 \
+		if (!(x))                                                                     \
+			std::cout << __FUNCTION__ << " failed on line " << __LINE__ << std::endl; \
+	}
 
 void test_same_hull()
 {
-    vector<Point_2> hull1 = { Point_2(0,0), Point_2(1,0), Point_2(1,1) };
-    vector<Point_2> hull2 = { Point_2(1,0), Point_2(1,1), Point_2(0,0) };
-    IS_TRUE(same_hull(hull1, hull2));
+	vector<Point_2> hull1 = {Point_2(0, 0), Point_2(1, 0), Point_2(1, 1)};
+	vector<Point_2> hull2 = {Point_2(1, 0), Point_2(1, 1), Point_2(0, 0)};
+	IS_TRUE(same_hull(hull1, hull2));
 
-    vector<Point_2> hull3 = { Point_2(0,0), Point_2(1,0) };
-    IS_TRUE(!same_hull(hull1, hull3));
+	vector<Point_2> hull3 = {Point_2(0, 0), Point_2(1, 0)};
+	IS_TRUE(!same_hull(hull1, hull3));
 }
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
-	using std::chrono::high_resolution_clock;
-	using std::chrono::duration_cast;
 	using std::chrono::duration;
+	using std::chrono::duration_cast;
+	using std::chrono::high_resolution_clock;
 	using std::chrono::milliseconds;
 	using AlgoType = std::function<void(std::vector<Point_2>::const_iterator, std::vector<Point_2>::const_iterator, std::back_insert_iterator<std::vector<Point_2>>)>;
 
 	AlgoType algo;
 
-    // Take CLI arg for what algo to run
+	// Take CLI arg for what algo to run
 	if (argc != 4)
 	{
 		std::cerr << "Must specify algorithm, number of points, and point distribution" << std::endl;
@@ -102,26 +145,35 @@ int main(int argc, char* argv[])
 	string num_points = argv[2];
 	string distribution = argv[3];
 
-	if (algo_arg == "jarvis") {
-		algo = [](auto f, auto l, auto o) { return jarvis(f, l, o); };	
-	} else if (algo_arg == "graham") {
-		algo = [](auto f, auto l, auto o) { return CGAL::ch_graham_andrew(f, l, o); };
-	} else if (algo_arg == "my_graham") {
-		algo = [](auto f, auto l, auto o) { return graham(f, l, o); };
+	if (algo_arg == "jarvis")
+	{
+		algo = [](auto f, auto l, auto o)
+		{ return jarvis(f, l, o); };
+	}
+	else if (algo_arg == "graham")
+	{
+		algo = [](auto f, auto l, auto o)
+		{ return CGAL::ch_graham_andrew(f, l, o); };
+	}
+	else if (algo_arg == "my_graham")
+	{
+		algo = [](auto f, auto l, auto o)
+		{ return graham(f, l, o); };
 	}
 	string input_filename = "input/" + distribution + "_" + num_points + ".txt";
 	std::vector<Point_2> hull;
 	std::vector<Point_2> pts;
 	std::ifstream in(input_filename);
-	if (!in) {
+	if (!in)
+	{
 		std::cerr << "Error: not a valid input\n";
 		return 1;
 	}
 	double x, y;
-	while (in >> x >> y) {
+	while (in >> x >> y)
+	{
 		pts.emplace_back(x, y);
 	}
-
 
 	auto t1 = std::chrono::high_resolution_clock::now();
 	algo(pts.begin(), pts.end(), std::back_inserter(hull));
@@ -132,27 +184,29 @@ int main(int argc, char* argv[])
 			  << " ms\n";
 	plot_hull(pts, hull);
 
-
 	// 1) Read all input points
 	SvgPlot plot;
-	for (auto& q : pts) plot.add_point(q.x(), q.y(), 3, "gray", "gray");
-    return 0;
+	for (auto &q : pts)
+		plot.add_point(q.x(), q.y(), 3, "gray", "gray");
+	return 0;
 }
 
 void plot_hull(std::vector<Point_2> points, std::vector<Point_2> hull)
 {
 	SvgPlot plot;
 	std::vector<std::pair<double, double>> poly;
-	for (auto& q : points) plot.add_point(q.x(), q.y(), 1, "gray", "gray");
-	for (auto& q : hull) plot.add_point(q.x(), q.y(), 2, "red", "red");
-	for (auto& q : hull) poly.push_back({q.x(), q.y()});
-	plot.add_polyline(poly, .02, "blue", true);
+	for (auto &q : points)
+		plot.add_point(q.x(), q.y(), 10, "gray", "gray");
+	for (auto &q : hull)
+		plot.add_point(q.x(), q.y(), 20, "red", "red");
+	for (auto &q : hull)
+		poly.push_back({q.x(), q.y()});
+	plot.add_polyline(poly, 2, "blue", true);
 	plot.write("debug.svg");
 	std::cout << "Wrote debug.svg\n";
-
 }
 
-vector<Point_2> jarvis_vector(const std::vector<Point_2>& points)
+vector<Point_2> jarvis_vector(const std::vector<Point_2> &points)
 {
 	Point_2 v1 = get_lowest(points);
 	Point_2 v0(-numeric_limits<double>::infinity(), v1.y());
@@ -162,7 +216,7 @@ vector<Point_2> jarvis_vector(const std::vector<Point_2>& points)
 	while (true)
 	{
 		optional<Point_2> p;
-		for (auto& q : points)
+		for (auto &q : points)
 		{
 			if (q != *(hull.rbegin() + 1) && q != hull.back())
 			{
@@ -179,7 +233,7 @@ vector<Point_2> jarvis_vector(const std::vector<Point_2>& points)
 					p = q;
 				}
 			}
-		}	
+		}
 		if (p == v1)
 		{
 			return vector<Point_2>(begin(hull) + 1, end(hull));
@@ -194,38 +248,60 @@ OutputIt jarvis(InputIt first, InputIt last, OutputIt out)
 {
 	std::vector<Point_2> points(first, last);
 	std::vector<Point_2> hull = jarvis_vector(points);
-	for (const auto& p : hull)
+	for (const auto &p : hull)
 		*out++ = p;
 	return out;
 }
 
+// Assumption: No duplicate points. We might see if we can handle this edge case
+// Warning: Probably buggy
 template <class InputIt, class OutputIt>
-OutputIt graham(InputIt first, InputIt last, OutputIt out) {
+OutputIt graham(InputIt first, InputIt last, OutputIt out)
+{
 	std::vector<Point_2> points(first, last);
-	std::sort(points.begin(), points.end());
+	Point_2 highest = get_highest(points);
+	std::sort(points.begin(), points.end(), make_upper_hull_comp(highest));
 	vector<Point_2> l_upper;
-	
+
 	l_upper.push_back(points[0]);
 	l_upper.push_back(points[1]);
 	for (int i = 3; i < points.size(); i++)
 	{
 		while (l_upper.size() > 1 &&
-					orientation(l_upper[l_upper.size() - 2], 
-					l_upper[l_upper.size() - 1], 
-					points[i]) != CGAL::RIGHT_TURN) {
+			   orientation(l_upper[l_upper.size() - 2],
+						   l_upper[l_upper.size() - 1],
+						   points[i]) != CGAL::RIGHT_TURN)
+		{
 			l_upper.pop_back();
 		}
 		l_upper.push_back(points[i]);
 	}
 
-	for (auto it = l_upper.begin(); it != l_upper.end(); ++it) {
+	size_t l_upper_size = l_upper.size();
+	for (int i = points.size() - 1; i >= 0; i--)
+	{
+		while (l_upper.size() > l_upper_size + 1 && orientation(l_upper[l_upper.size() - 2],
+																l_upper[l_upper.size() - 1],
+																points[i]) != CGAL::RIGHT_TURN)
+		{
+			l_upper.pop_back();
+		}
+		l_upper.push_back(points[i]);
+	}
+
+	for (auto it = l_upper.begin(); it != l_upper.end(); ++it)
+	{
 		*out++ = *it;
 	}
 	return out;
 }
 
 // Tests
-#define IS_TRUE(x) { if (!(x)) std::cout << __FUNCTION__ << " failed on line " << __LINE__ << std::endl; }
+#define IS_TRUE(x)                                                                    \
+	{                                                                                 \
+		if (!(x))                                                                     \
+			std::cout << __FUNCTION__ << " failed on line " << __LINE__ << std::endl; \
+	}
 
 std::vector<Point_2> test_cgal_graham(std::vector<Point_2> pts)
 {
